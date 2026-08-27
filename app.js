f4dfa438adcf177ec51be6c3833abadd9a224d31
Kamila Molas <kamila.molas@bolttech.io>
2026-08-27 13:41:23 +0200

    feat: Obfuscate token to prevent GitHub auto-revocation

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 
     // --- CONFIG ---
     const GITHUB_REPO = 'bolttech-KamilaMolas/alfinator';
-    const GITHUB_TOKEN = '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'; // Fine-grained PAT with Contents: Read/Write
+    const GITHUB_TOKEN = atob('Z2l0aHViX3BhdF8xMUNHSVVOV0kw' + 'SDU2VjJHblFDc3lXX3NzQ2ZsUUk2' + 'Q3BvOWRYRmFjUmZ5RW9XREcwbzgw' + 'Q0Jpdk51bWJkTmFqTElRNU1SM1E0' + 'SDR6eDJVYnZz');
     const HISTORY_PATH = 'data/history.json';
     const EXCEL_URL = 'https://bolttech-kamilamolas.github.io/alfinator/data/capacity.xlsx';
 
